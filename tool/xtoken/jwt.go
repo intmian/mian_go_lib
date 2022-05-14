384b65b92eadfaf1402ab2d92cc39c4dd6080e94
mian <38103855+intmian@users.noreply.github.com>
2022-05-14 13:41:34 +0800

    新增token模块

---
+++ tool/xtoken/jwt.go
@@ -0,0 +1,69 @@
+package xtoken
+
+import (
+	"github.com/intmian/mian_go_lib/tool/cipher"
+	"strconv"
+	"strings"
+	"sync"
+	"time"
+)
+
+type JwtMgr struct {
+	salt1 string
+	salt2 string
+	rwL   sync.RWMutex
+}
+
+func NewJwtMgr(salt1 string, salt2 string) *JwtMgr {
+	m := &JwtMgr{salt1: salt1, salt2: salt2}
+	m.rwL = sync.RWMutex{}
+	return m
+}
+
+type Data struct {
+	User       string
+	Permission []string
+	ValidTime  int64 // 时间戳
+	token      string
+}
+
+func (m *JwtMgr) SetSalt(salt1, salt2 string) {
+	// 上个锁。避免出现生成时一半是salt1，一半是salt2的情况
+	m.rwL.Lock()
+	defer m.rwL.Unlock()
+	m.salt1 = salt1
+	m.salt2 = salt2
+}
+
+func (m *JwtMgr) GenToken(user string, permission []string, validTime int64) string {
+	m.rwL.RLock()
+	defer m.rwL.RUnlock()
+	s1 := m.salt1
+	s2 := m.salt2
+	key := user + strconv.FormatInt(validTime, 10) + strings.Join(permission, "")
+	token := cipher.Sha2562String(s1 + key)
+	token = cipher.Sha2562String(s2 + token)
+	return token
+}
+
+func (m *JwtMgr) CheckSignature(data *Data, now time.Time, wantPermission string) bool {
+	// 时间戳
+	if data.ValidTime < now.Unix() {
+		return false
+	}
+	// 检查有效性
+	if m.GenToken(data.User, data.Permission, data.ValidTime) != data.token {
+		return false
+	}
+	// 如果没有权限，则返回false
+	for _, p := range data.Permission {
+		if p == wantPermission {
+			return true
+		}
+	}
+	return false
+}
+
+func (m *JwtMgr) Signature(data *Data) {
+	data.token = m.GenToken(data.User, data.Permission, data.ValidTime)
+}
---
+++ tool/xtoken/jwt_test.go
@@ -0,0 +1,81 @@
+package xtoken
+
+import (
+	"testing"
+	"time"
+)
+
+func TestJwt(t *testing.T) {
+	a := NewJwtMgr("1", "2")
+	d := Data{
+		User:       "test",
+		Permission: []string{"read", "write"},
+		ValidTime:  time.Now().Add(time.Hour * 24 * 7).Unix(),
+		token:      "",
+	}
+	a.Signature(&d)
+
+	tests := []struct {
+		name       string
+		time       int64
+		permission string
+		want       bool
+	}{
+		{
+			name:       "right permission, in time",
+			time:       time.Now().Add(time.Hour * 24 * 6).Unix(),
+			permission: "read",
+			want:       true,
+		},
+		{
+			name:       "right permission, in time",
+			time:       time.Now().Add(time.Hour * 24 * 6).Unix(),
+			permission: "write",
+			want:       true,
+		},
+		{
+			name:       "wrong permission, in time",
+			time:       time.Now().Add(time.Hour * 24 * 6).Unix(),
+			permission: "delete",
+			want:       false,
+		},
+		{
+			name:       "right permission, out time",
+			time:       time.Now().Add(time.Hour * 24 * 8).Unix(),
+			permission: "read",
+			want:       false,
+		},
+		{
+			name:       "right permission, out time",
+			time:       time.Now().Add(time.Hour * 24 * 8).Unix(),
+			permission: "write",
+			want:       false,
+		},
+		{
+			name:       "wrong permission, out time",
+			time:       time.Now().Add(time.Hour * 24 * 8).Unix(),
+			permission: "delete",
+			want:       false,
+		},
+	}
+
+	for _, test := range tests {
+		t2 := time.Unix(test.time, 0)
+		t.Run(test.name, func(ttt *testing.T) {
+			if got := a.CheckSignature(&d, t2, test.permission); got != test.want {
+				ttt.Errorf("Check(%v, %v) = %v, want %v\n", d.token, test.permission, got, test.want)
+			} else {
+				ttt.Logf("Check(%v, %v) = %v, want %v\n", d.token, test.permission, got, test.want)
+			}
+		})
+	}
+
+	d.Permission = append(d.Permission, "delete")
+	t.Run("fake permission, in time", func(ttt *testing.T) {
+		if got := a.CheckSignature(&d, time.Now().Add(time.Hour*24*6), "delete"); got != false {
+			ttt.Errorf("Check(%v, %v) = %v, want %v\n", d.token, "fake", got, false)
+		} else {
+			ttt.Logf("Check(%v, %v) = %v, want %v\n", d.token, "fake", got, false)
+		}
+	})
+}
