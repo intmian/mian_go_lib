5a4109ed89c9c10d0bd1d6055bd516d6e148067e
mian <38103855+intmian@users.noreply.github.com>
2022-07-02 20:31:25 +0800

    提交一些
    Signed-off-by: mian <38103855+intmian@users.noreply.github.com>

--- tool/xres/define.go
+++ tool/xres/define.go
@@ -1,15 +1,17 @@
 package xres
 
+//ColumnType 列类型
 type ColumnType int
 
 const (
-	CtInt ColumnType = iota
-	CtString
-	CtFloat
-	CtEnum
-	CtVecDataPKey
-	CtVecDataCKey
-	CtVecDataValue
+	CtNone         ColumnType = iota //无效
+	CtInt                            //整数
+	CtString                         //文本
+	CtFloat                          //小数
+	CtEnum                           //枚举
+	CtVecDataPKey                    //主枚举列
+	CtVecDataCKey                    //子枚举列
+	CtVecDataValue                   //数据列
 )
 
 type ExcelCowMeta struct {
@@ -18,5 +20,26 @@ type ExcelCowMeta struct {
 }
 
 type ExcelCowsMeta struct {
-	Columns map[string]ExcelCowMeta
+	Columns map[string]*ExcelCowMeta
+}
+
+func (m *ExcelCowsMeta) GetColumnType() ColumnType {
+	switch m.Columns["type"].Data {
+	case "整数":
+		return CtInt
+	case "文本":
+		return CtString
+	case "小数":
+		return CtFloat
+	case "枚举":
+		return CtEnum
+	case "主枚举列":
+		return CtVecDataPKey
+	case "子枚举列":
+		return CtVecDataCKey
+	case "数据列":
+		return CtVecDataValue
+	default:
+		return CtNone
+	}
 }
