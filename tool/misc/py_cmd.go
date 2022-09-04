2998a80fb9e80db5b794dd69f33780c7bd3931d6
mian <38103855+intmian@users.noreply.github.com>
2022-09-04 11:46:38 +0800

    拓展一下python框架、加强鲁棒性

--- tool/misc/py_cmd.go
+++ tool/misc/py_cmd.go
@@ -7,7 +7,16 @@ import (
 func CmdPy(pythonPath string, cmdArgs ...string) (result string, err error) {
 	args := []string{pythonPath}
 	args = append(args, cmdArgs...)
-	out, err := exec.Command("python", args...).Output()
+	pys := []string{"python", "python3"}
+	usePy := ""
+	for _, py := range pys {
+		_, err = exec.LookPath(py)
+		if err == nil {
+			usePy = py
+			break
+		}
+	}
+	out, err := exec.Command(usePy, args...).Output()
 	if err != nil {
 		return "", err
 	}
