dddc57538d93503f157a7b4ca9ec4db7219ed2f2
mian <38103855+intmian@users.noreply.github.com>
2023-10-26 17:25:42 +0800

    增加一堆小工具，同时整理下这个文件

--- tool/misc/small_tool.go
+++ tool/misc/small_tool.go
@@ -3,18 +3,22 @@ package misc
 import (
 	"bufio"
 	"fmt"
+	"golang.org/x/text/encoding/simplifiedchinese"
+	"golang.org/x/text/transform"
+	"io"
 	"os"
 	"os/exec"
+	"path/filepath"
 	"strings"
 	"time"
 )
 
-//Str2list 将json风格的数组字符串s转换为字符串
+// Str2list 将json风格的数组字符串s转换为字符串
 func Str2list(s string) []string {
 	return strings.Split(s[1:len(s)-1], ",")
 }
 
-//MinMaxInt 求一个int切片的最大最小值
+// MinMaxInt 求一个int切片的最大最小值
 func MinMaxInt(array []int) (min int, minIndex int, max int, maxIndex int) {
 	max = array[0]
 	min = array[0]
@@ -31,7 +35,7 @@ func MinMaxInt(array []int) (min int, minIndex int, max int, maxIndex int) {
 	return
 }
 
-//MinMaxUInt 求一个int切片的最大最小值
+// MinMaxUInt 求一个int切片的最大最小值
 func MinMaxUInt(array []uint32) (min uint32, minIndex int, max uint32, maxIndex int) {
 	max = array[0]
 	min = array[0]
@@ -52,7 +56,7 @@ type CanCompare interface {
 	Less(CanCompare) bool
 }
 
-//MinMax 求一个CanCompare切片的最大最小值
+// MinMax 求一个CanCompare切片的最大最小值
 func MinMax(array []CanCompare) (min CanCompare, minIndex int, max CanCompare, maxIndex int) {
 	max = array[0]
 	min = array[0]
@@ -69,7 +73,7 @@ func MinMax(array []CanCompare) (min CanCompare, minIndex int, max CanCompare, m
 	return
 }
 
-//GetTimeStr 返回当前的时间，格式为2006-01-02 15:04:05
+// GetTimeStr 返回当前的时间，格式为2006-01-02 15:04:05
 func GetTimeStr() string {
 	return time.Now().Format("2006-01-02 15:04:05")
 }
@@ -109,7 +113,7 @@ func ClearIOBuffer() {
 	_, _ = myReader.ReadString('\n')
 }
 
-//IsLegalOutURL 判断是否是合法的外链
+// IsLegalOutURL 判断是否是合法的外链
 func IsLegalOutURL(url string) bool {
 	if len(url) < 6 {
 		return false
@@ -121,7 +125,7 @@ func IsLegalOutURL(url string) bool {
 	return true
 }
 
-//Clear 清空屏幕
+// Clear 清空屏幕
 func Clear() {
 	cmd := exec.Command("cmd.exe", "/c", "cls")
 	cmd.Stdout = os.Stdout
@@ -129,4 +133,98 @@ func Clear() {
 	if err != nil {
 		print("clear fail")
 	}
+}
+
+// GetRealDir 返回当前可执行程序的真正目录，如果是软链接，则返回软链接指向的文件的目录
+func GetRealDir() string {
+	// 获取当前可执行程序的路径
+	exePath, _ := os.Executable()
+	// 获取当前可执行程序的目录
+	exeDir := filepath.Dir(exePath)
+	// 如果是软链接，获取软链接的路径
+	if linkPath, err := os.Readlink(exePath); err == nil {
+		// 如果是软链接，则获取软链接的绝对路径
+		linkDir := filepath.Dir(linkPath)
+		exeDir = linkDir
+	}
+	return exeDir
+}
+
+func InputString(notice string) string {
+	fmt.Print(notice)
+	var fileNames string
+	_, err := fmt.Scanln(&fileNames)
+	if err != nil {
+		return ""
+	}
+	return fileNames
+}
+
+func InputStringWithSpace(notice string) []string {
+	var msg string
+	fmt.Print(notice)
+	reader := bufio.NewReader(os.Stdin)
+	msg, _ = reader.ReadString('\n')
+	msg = strings.TrimSpace(msg)
+	msgList := strings.Split(msg, " ")
+	return msgList
+}
+
+func InputInt(notice string) int {
+	fmt.Print(notice)
+	var choice int
+	_, err := fmt.Scanln(&choice)
+	if err != nil {
+		return 0
+	}
+	return choice
+}
+
+func ReadGBKFileLine(addr string) (string, error) {
+	file, err := os.Open(addr)
+	if err != nil {
+		return "", err
+	}
+	defer file.Close()
+
+	reader := bufio.NewReader(file)
+	decoder := simplifiedchinese.GB18030.NewDecoder()
+	returnStr := ""
+	for {
+		line, err := reader.ReadString('\n')
+		if err == io.EOF && len(line) == 0 {
+			break
+		}
+		// 去除末尾的\r\n
+		line = strings.TrimSuffix(line, "\n")
+		line = strings.TrimSuffix(line, "\r")
+
+		line, _, err = transform.String(decoder, line)
+		if err != nil {
+			return "", err
+		}
+		returnStr += line
+	}
+	return returnStr, nil
+}
+
+func WriteGBKFileLine(addr string, content string) error {
+	file, err := os.Create(addr)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	encoder := simplifiedchinese.GB18030.NewEncoder()
+
+	encodedLine, err := encoder.String(content)
+	if err != nil {
+		return err
+	}
+	_, err = file.WriteString(encodedLine + "\r\n")
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
