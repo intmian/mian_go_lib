bfa91f8f35556f0fcdf56491244735efe606d538
mian <38103855+intmian@users.noreply.github.com>
2024-03-14 11:37:51 +0800

    给接口做了一个退化功能，能够自动化降级

--- tool/ai/openai.go
+++ tool/ai/openai.go
@@ -2,12 +2,13 @@ package ai
 
 import (
 	"context"
+	"errors"
 	openai "github.com/sashabaranov/go-openai"
 )
 
 type OpenAI struct {
 	cl     *openai.Client
-	model  string
+	model  []string
 	renshe string
 }
 
@@ -24,31 +25,43 @@ func (o *OpenAI) Init(baseUrl, token string, cheap bool, renshe string) {
 	config.BaseURL = baseUrl
 	o.cl = openai.NewClientWithConfig(config)
 	if cheap {
-		o.model = "gpt-3.5-turbo"
+		o.model = []string{"gpt-3.5-turbo"}
 	} else {
-		o.model = "gpt-4"
+		o.model = []string{"gpt-4", "gpt-4-0125-preview"}
 	}
 }
 
 func (o *OpenAI) Chat(content string) (string, error) {
-	resp, err := o.cl.CreateChatCompletion(
-		context.Background(),
-		openai.ChatCompletionRequest{
-			Model: o.model,
-			Messages: []openai.ChatCompletionMessage{
-				{
-					Role:    openai.ChatMessageRoleSystem,
-					Content: o.renshe,
-				},
-				{
-					Role:    openai.ChatMessageRoleUser,
-					Content: content,
+	suc := false
+	var err error
+	var resp openai.ChatCompletionResponse
+	for _, model := range o.model {
+		resp, err = o.cl.CreateChatCompletion(
+			context.Background(),
+			openai.ChatCompletionRequest{
+				Model: model,
+				Messages: []openai.ChatCompletionMessage{
+					{
+						Role:    openai.ChatMessageRoleSystem,
+						Content: o.renshe,
+					},
+					{
+						Role:    openai.ChatMessageRoleUser,
+						Content: content,
+					},
 				},
 			},
-		},
-	)
-	if err != nil {
-		return "", err
+		)
+		if err == nil && resp.Choices[0].Message.Content != "" {
+			suc = true
+			break
+		}
+	}
+	if !suc {
+		if err != nil {
+			return "", err
+		}
+		return "", errors.New("openai-empty" + string(resp.Choices[0].FinishReason))
 	}
 	return resp.Choices[0].Message.Content, nil
 }
