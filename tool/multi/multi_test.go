68b69926177cbf73fd04b55c1f39c8f07322fa12
mian <38103855+intmian@users.noreply.github.com>
2024-03-29 14:00:21 +0800

    继续强化多线程单元锁库

--- tool/multi/multi_test.go
+++ tool/multi/multi_test.go
@@ -38,41 +38,85 @@ func TestSafeMap(t *testing.T) {
 }
 
 func TestUnitLock(t *testing.T) {
-	u := NewUnitLock[string]()
-	wg := sync.WaitGroup{}
-	for i := 0; i < 100; i++ {
-		wg.Add(1)
-		go func(i int) {
-			u.Lock(fmt.Sprintf("key"))
-			defer u.Unlock(fmt.Sprintf("key"))
-			wg.Done()
-		}(i)
-	}
-	c := make(chan any)
-	go func() {
-		wg.Wait()
-		c <- nil
-	}()
-	select {
-	case <-c:
-	case <-time.After(time.Second):
-		t.Error("lock error")
-	}
-	for i := 0; i < 100; i++ {
-		wg.Add(1)
-		go func(i int) {
-			u.SafeRun(fmt.Sprintf("key"), func() {
+	t.Run("1", func(t *testing.T) {
+		u := NewUnitLock[string]()
+		wg := sync.WaitGroup{}
+		for i := 0; i < 100; i++ {
+			wg.Add(1)
+			go func(i int) {
+				u.Lock(fmt.Sprintf("key"))
+				defer u.Unlock(fmt.Sprintf("key"))
+				wg.Done()
+			}(i)
+		}
+		c := make(chan any)
+		go func() {
+			wg.Wait()
+			c <- nil
+		}()
+		select {
+		case <-c:
+		case <-time.After(time.Second):
+			t.Error("lock error")
+		}
+		for i := 0; i < 100; i++ {
+			wg.Add(1)
+			go func(i int) {
+				u.SafeRun(fmt.Sprintf("key"), func() {
+					wg.Done()
+				})
+			}(i)
+		}
+		go func() {
+			wg.Wait()
+			c <- nil
+		}()
+		select {
+		case <-c:
+		case <-time.After(time.Second):
+			t.Error("lock error")
+		}
+	})
+	t.Run("lock all", func(t *testing.T) {
+		n := time.Now()
+		u := NewUnitLock[string]()
+		wg := sync.WaitGroup{}
+		for i := 0; i < 100; i++ {
+			wg.Add(1)
+			go u.SafeRun(fmt.Sprintf("key %d", i), func() {
 				wg.Done()
 			})
-		}(i)
-	}
-	go func() {
-		wg.Wait()
-		c <- nil
-	}()
-	select {
-	case <-c:
-	case <-time.After(time.Second):
-		t.Error("lock error")
-	}
+		}
+		for i := 0; i < 100; i++ {
+			wg.Add(1)
+			i := i
+			go func() {
+				time.Sleep(time.Second)
+				u.SafeRun(fmt.Sprintf("key %d", i), func() {
+					if time.Since(n) < time.Second*10 {
+						t.Error("lock error")
+					}
+					wg.Done()
+				})
+			}()
+		}
+		wg.Add(1)
+		go func() {
+			u.SafeRunAll(func() {
+				time.Sleep(time.Second * 10)
+				wg.Done()
+			})
+		}()
+		timeout := time.After(time.Second * 12)
+		c := make(chan any)
+		go func() {
+			wg.Wait()
+			c <- nil
+		}()
+		select {
+		case <-c:
+		case <-timeout:
+			t.Error("lock error")
+		}
+	})
 }
--- tool/multi/unitlock.go
+++ tool/multi/unitlock.go
@@ -4,29 +4,77 @@ import "sync"
 
 // UnitLock 支持锁定某个key，可以融合进一些复杂的逻辑中
 type UnitLock[KeyType comparable] struct {
-	m map[KeyType]*sync.Mutex
-	l sync.Mutex
+	m map[KeyType]*sync.RWMutex
+	// 锁住map的操作
+	mapLock sync.Mutex
+	// 用来锁定所有数据的操作
+	allLock sync.RWMutex
 }
 
 func NewUnitLock[KeyType comparable]() *UnitLock[KeyType] {
-	return &UnitLock[KeyType]{m: make(map[KeyType]*sync.Mutex)}
+	return &UnitLock[KeyType]{m: map[KeyType]*sync.RWMutex{}}
 }
 
 func (u *UnitLock[KeyType]) Lock(key KeyType) {
-	u.l.Lock()
+	u.allLock.RLock()
+	u.mapLock.Lock()
 	if _, ok := u.m[key]; !ok {
-		u.m[key] = &sync.Mutex{}
+		u.m[key] = &sync.RWMutex{}
 	}
-	u.l.Unlock()
+	u.mapLock.Unlock()
 	u.m[key].Lock()
 }
 
 func (u *UnitLock[KeyType]) Unlock(key KeyType) {
+	u.allLock.RUnlock()
+	u.mapLock.Lock()
 	u.m[key].Unlock()
+	u.mapLock.Unlock()
+}
+
+func (u *UnitLock[KeyType]) RLock(key KeyType) {
+	u.allLock.RLock()
+	u.mapLock.Lock()
+	if _, ok := u.m[key]; !ok {
+		u.m[key] = &sync.RWMutex{}
+	}
+	u.mapLock.Unlock()
+	u.m[key].RLock()
+}
+
+func (u *UnitLock[KeyType]) RUnlock(key KeyType) {
+	u.allLock.RUnlock()
+	u.mapLock.Lock()
+	u.m[key].RUnlock()
+	u.mapLock.Unlock()
 }
 
 func (u *UnitLock[KeyType]) SafeRun(key KeyType, f func()) {
+	u.allLock.RLock()
+	defer u.allLock.RUnlock()
 	u.Lock(key)
 	defer u.Unlock(key)
+	f()
+}
+
+func (u *UnitLock[KeyType]) SafeRunR(key KeyType, f func()) {
+	u.allLock.RLock()
+	defer u.allLock.RUnlock()
+	u.RLock(key)
+	defer u.RUnlock(key)
+	f()
+}
+
+func (u *UnitLock[KeyType]) LockAll() {
+	u.allLock.Lock()
+}
+
+func (u *UnitLock[KeyType]) UnlockAll() {
+	u.allLock.Unlock()
+}
+
+func (u *UnitLock[KeyType]) SafeRunAll(f func()) {
+	u.LockAll()
+	defer u.UnlockAll()
 	f()
 }
