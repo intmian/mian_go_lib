fb7aae755c301e93fe71d799bc7cfc169856b4b9
mian <38103855+intmian@users.noreply.github.com>
2024-04-11 15:17:17 +0800

    增加一个配置相关的cfg包装

---
+++ xstorage/cfg_pack.go
@@ -0,0 +1,121 @@
+package xstorage
+
+import (
+	"github.com/intmian/mian_go_lib/tool/misc"
+)
+
+type CfgParam struct {
+	Key       string // 与前端协调的key
+	RealKey   string // storage里面的key
+	ValueType ValueType
+}
+
+// CfgExt 为xstorage增加一个通用的配置机制
+type CfgExt struct {
+	core     *XStorage
+	paramMap map[string]*CfgParam
+	initTag  misc.InitTag
+}
+
+func (c *CfgExt) Init(core *XStorage) error {
+	if core == nil {
+		return ErrCoreIsNil
+	}
+	c.paramMap = make(map[string]*CfgParam)
+	c.core = core
+	c.initTag.SetInitialized()
+	return nil
+}
+
+func NewCfgExt(core *XStorage) (*CfgExt, error) {
+	ret := &CfgExt{}
+	err := ret.Init(core)
+	if err != nil {
+		return nil, err
+	}
+	return ret, nil
+}
+
+func (c *CfgExt) AddParam(param *CfgParam) error {
+	if !c.initTag.IsInitialized() {
+		return ErrNotInitialized
+	}
+	// 校验key、以及各个参数的合法性
+	if param == nil {
+		return ErrParamIsNil
+	}
+	if param.Key == "" || param.RealKey == "" || param.ValueType == 0 {
+		return ErrParamIsInvalid
+	}
+	_, ok := c.paramMap[param.Key]
+	if ok {
+		return ErrKeyAlreadyExist
+	}
+	c.paramMap[param.Key] = param
+	return nil
+}
+
+func (c *CfgExt) SetCfg(key string, value ValueUnit) error {
+	if !c.initTag.IsInitialized() {
+		return ErrNotInitialized
+	}
+	if key == "" {
+		return ErrParamIsEmpty
+	}
+	param, ok := c.paramMap[key]
+	if !ok {
+		return ErrKeyNotFound
+	}
+	if value.Type != param.ValueType {
+		return ErrValueTypeNotMatch
+	}
+	return c.core.Set(Join("cfg", param.RealKey), &value)
+}
+
+func (c *CfgExt) SetUserCfg(user string, key string, value ValueUnit) error {
+	if !c.initTag.IsInitialized() {
+		return ErrNotInitialized
+	}
+	if user == "" || key == "" {
+		return ErrParamIsEmpty
+	}
+	param, ok := c.paramMap[key]
+	if !ok {
+		return ErrKeyNotFound
+	}
+	userKey := Join("cfg", user, param.RealKey)
+	if value.Type != param.ValueType {
+		return ErrValueTypeNotMatch
+	}
+	return c.core.Set(userKey, &value)
+}
+
+func (c *CfgExt) GetAllCfg() (map[string]ValueUnit, error) {
+	if !c.initTag.IsInitialized() {
+		return nil, ErrNotInitialized
+	}
+	ret := make(map[string]ValueUnit)
+	for k, v := range c.paramMap {
+		value, err := c.core.Get(Join("cfg", v.RealKey))
+		if err != nil {
+			return nil, err
+		}
+		ret[k] = *value
+	}
+	return ret, nil
+}
+
+func (c *CfgExt) GetUserCfg(user string) (map[string]ValueUnit, error) {
+	if !c.initTag.IsInitialized() {
+		return nil, ErrNotInitialized
+	}
+	ret := make(map[string]ValueUnit)
+	for k, v := range c.paramMap {
+		value, err := c.core.Get(Join("cfg", user, v.RealKey))
+		if err != nil {
+			return nil, err
+		}
+		ret[k] = *value
+	}
+	return ret, nil
+}
--- xstorage/err_auto.go
+++ xstorage/err_auto.go
@@ -5,6 +5,13 @@ type ErrStr string
 const(
     ErrNil = ErrStr("nil")
     ErrValueTypeNotMatch = ErrStr("value type not match")  // auto generated from .\sqlite.go
+    ErrCoreIsNil = ErrStr("core is nil")  // auto generated from .\cfg_pack.go
+    ErrNotInitialized = ErrStr("not initialized")  // auto generated from .\cfg_pack.go
+    ErrKeyNotFound = ErrStr("key not found")  // auto generated from .\cfg_pack.go
+    ErrParamIsNil = ErrStr("param is nil")  // auto generated from .\cfg_pack.go
+    ErrParamIsInvalid = ErrStr("param is invalid")  // auto generated from .\cfg_pack.go
+    ErrKeyAlreadyExist = ErrStr("key already exist")  // auto generated from .\cfg_pack.go
+    ErrParamIsEmpty = ErrStr("param is empty")  // auto generated from .\cfg_pack.go
 )
 
 func (e ErrStr) Error() string { return string(e) }
