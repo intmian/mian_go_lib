9417bad1b5115922dc7ac2d100e442d23ef8539c
mian <38103855+intmian@users.noreply.github.com>
2024-07-15 19:04:55 +0800

    增加debug代码

--- tool/spider/baidu.go
+++ tool/spider/baidu.go
@@ -157,12 +157,12 @@ func getBaiduNewsPage(keyword string, page int) (result []BaiduNew, err error) {
 		bn.href = result2[5]
 		result = append(result, bn)
 	}
-	if len(result) == 0 && page <= 1 {
-		// 一般不会第一页都没有数据。报错+打印内容方便debug。
-		f, _ := os.Create(fmt.Sprintf("baidu_%s_%d_%s.html", keyword, page, time.Now().Format("2006-01-02_15:04:05")))
-		f.WriteString(string(webText))
-		return nil, errors.New("no news")
-	}
+	//if len(result) == 0 && page <= 1 {
+	// 加入全量打印用于调试。报错+打印内容方便debug。
+	f, _ := os.Create(fmt.Sprintf("baidu_%s_%d_%s.html", keyword, page, time.Now().Format("2006-01-02_15:04:05")))
+	f.WriteString(string(webText))
+	return nil, errors.New("no news")
+	//}
 	return
 }
 
