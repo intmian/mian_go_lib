f3e8b76f24ebf00c3444d62a90893232479a689f
mian <38103855+intmian@users.noreply.github.com>
2025-05-09 17:03:27 +0800

    上传一个set

---
+++ tool/misc/set.go
@@ -0,0 +1,44 @@
+package misc
+
+type Set[T comparable] map[T]struct{}
+
+func NewSet[T comparable]() Set[T] {
+	return make(Set[T])
+}
+
+func (s Set[T]) Add(item T) {
+	s[item] = struct{}{}
+}
+
+func (s Set[T]) Remove(item T) {
+	delete(s, item)
+}
+
+func (s Set[T]) Contains(item T) bool {
+	_, exists := s[item]
+	return exists
+}
+
+func (s Set[T]) Size() int {
+	return len(s)
+}
+
+func (s Set[T]) Clear() {
+	for k := range s {
+		delete(s, k)
+	}
+}
+
+func (s Set[T]) ToSlice() []T {
+	slice := make([]T, 0, len(s))
+	for k := range s {
+		slice = append(slice, k)
+	}
+	return slice
+}
+
+func (s Set[T]) FromSlice(slice []T) {
+	for _, item := range slice {
+		s.Add(item)
+	}
+}
